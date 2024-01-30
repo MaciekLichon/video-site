4777863e600214ce0f9477a02e4ff6b8eed5cc3f
maciejlichon <maciej.lichon@geckodynamics.com>
2024-01-30 15:57:06 +0100

    loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router";
 
+import Loading from "./components/pages/Loading/Loading";
 import About from "./components/pages/About/About";
 import Awards from "./components/pages/Awards/Awards";
 import Home from "./components/pages/Home/Home";
@@ -13,20 +14,26 @@ import Header from "./components/common/Header/Header";
 import VideoPage from "./components/pages/Video/VideoPage";
 
 const App: React.FC = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [previewFinished, setPreviewFinished] = useState(false);
+
     return (
         <>
-            <Header />
-            <Routes>
-                <Route path="" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/awards" element={<Awards />} />
-                <Route path="/people" element={<People />} />
-                <Route path="/videos" element={<Videos />} />
-                <Route path="/videos/:id" element={<VideoPage />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-            <Footer />
+            {/* {(isLoading || !previewFinished) && <Loading setPreviewFinished={setPreviewFinished} />} */}
+            <>
+                <Header />
+                <Routes>
+                    <Route path="" element={<Home setIsLoading={setIsLoading} />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/awards" element={<Awards />} />
+                    <Route path="/people" element={<People />} />
+                    <Route path="/videos" element={<Videos />} />
+                    <Route path="/videos/:id" element={<VideoPage />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+                <Footer />
+            </>
         </>
     );
 };
--- src/components/pages/People/Person/Person.tsx
+++ src/components/pages/People/Person/Person.tsx
@@ -1,16 +1,61 @@
 import "./Person.scss";
 
 import React from "react";
+import { motion } from "framer-motion";
 
 import { IPerson } from "../../../../data/data";
+
+const pVariants = {
+    hidden: {
+        y: 15,
+        opacity: 0,
+    },
+    visible: {
+        y: 0,
+        opacity: 1,
+    },
+};
 
 const Person: React.FC<IPerson> = ({ image, name, title, description }) => {
     return (
         <div className="person">
-            <img src={image} alt="person image" className="person__img" />
-            <p className="person__name">{name}</p>
-            <p className="person__title">{title}</p>
-            <p className="person__description">{description}</p>
+            <motion.img
+                initial={{ scale: 1.2 }}
+                whileInView={{ scale: 1 }}
+                transition={{
+                    duration: 1.5,
+                }}
+                src={image}
+                alt="person image"
+                className="person__img"
+            />
+            <motion.p
+                initial="hidden"
+                whileInView="visible"
+                variants={pVariants}
+                transition={{ duration: 1 }}
+                className="person__name"
+            >
+                {name}
+            </motion.p>
+            <motion.p
+                initial="hidden"
+                whileInView="visible"
+                variants={pVariants}
+                transition={{ duration: 1 }}
+                className="person__title"
+            >
+                {title}
+            </motion.p>
+            <motion.p
+                initial="hidden"
+                whileInView="visible"
+                variants={pVariants}
+                transition={{ duration: 1 }}
+                className="person__description"
+            >
+                {description}
+            </motion.p>
         </div>
     );
 };
